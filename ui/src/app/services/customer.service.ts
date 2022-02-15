44b5f1e33bf6084a3fb7678958f8935b24c563b6
Nare Sai Chaitanya <62926225+naresaichaitanya@users.noreply.github.com>
2022-02-15 20:59:05 +0530

    [internal] added customer model & service file

--- ui/src/app/services/customer.service.ts
+++ ui/src/app/services/customer.service.ts
@@ -1,16 +1,65 @@
-import { Injectable } from '@angular/core'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Constants } from '../helpers/constants';
+import { Customer } from '../model/customer.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private readonly API_URL: string = environment.apiEndpoint + '/customers';
+
   private selectedCustomer: any;
-  constructor() { }
-
+  constructor(private httpClient: HttpClient) { }
+  //set the selected customer
   setSelectedCustomer(customer: any) { this.selectedCustomer = customer; }
-
+  //get the selected customer
   getSelectedCustomer() {
     return (this.selectedCustomer) ? this.selectedCustomer : JSON.parse(localStorage.getItem(Constants.SELECTED_CUSTOMER));
+  }
+  /**
+   * create new customer
+   * @param customerName: string 
+   * @returns 
+   */
+  public createCustomer(customerName: string) {
+    return this.httpClient.post(this.API_URL, { customerName });
+  }
+
+  /**
+   * fetch customer details list
+   */
+  public getCustomerList() {
+    const headers = this.getHeaders();
+    return this.httpClient.get<Customer>(this.API_URL, { headers });
+  }
+  /**
+   * update customer details
+   * @param customer: Customer 
+   * @returns 
+   */
+  public updateCustomer(customer: Customer) {
+    return this.httpClient.put(`${this.API_URL}/${customer.customerId}`, customer);
+  }
+
+  /**
+   * delete selected customer by customerId
+   * @param customerId: string 
+   */
+  public deleteCustomer(customerId: string) {
+    return this.httpClient.delete(`${this.API_URL}/${customerId}`);
+  }
+
+
+
+  /**
+   * set the header for the request
+   * @returns HttpHeaders 
+   */
+  public getHeaders() {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return headers;
   }
 }
