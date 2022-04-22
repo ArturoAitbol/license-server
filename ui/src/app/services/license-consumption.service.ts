f3a412da7f95258d21ddf52a2bf68edfe6da2f70
Paolo Fernandez <pfernandez@aitbol.com>
2022-04-21 21:25:28 -0400

    modifying frontend code to match APIs changes

---
+++ ui/src/app/services/license-consumption.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Constants } from '../helpers/constants';
+import { LicenseConsumption } from '../model/license-consumption.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LicenseConsumptionService {
+  private readonly API_URL: string = environment.apiEndpoint + '/licenseUsageDetails';
+
+  constructor(private httpClient: HttpClient) { }
+
+  /**
+   * add License Usage details
+   * @param data: LicenseConsumption
+   * @returns: Observable 
+   */
+  public addLicenseConsumptionDetails(data: LicenseConsumption) {
+    return this.httpClient.post(this.API_URL, data);
+  }
+  /**
+   * get particular LicenseConsumption details by licenseId
+   * @param licenseId: string 
+   * @returns: Observable 
+   */
+  public getLicenseDetails(data: any) {
+    const headers = this.getHeaders();
+    let params = new HttpParams()
+        .set('subaccount-id', data.subaccount)
+        .set('view', data.view);
+    if (data.year)
+      params = params.set('year', data.year);
+    if (data.month)
+      params = params.set('month', data.month);
+    if (data.startDate && data.endDate) {
+      params = params.set('startDate', data.startDate);
+      params = params.set('endDate', data.endDate);
+    }
+    return this.httpClient.get(this.API_URL, { headers, params });
+  }
+  /**
+   * update License Usage details
+   * @param data: LicenseConsumption 
+   * @returns: Observable 
+   */
+  public updateLicenseConsumptionDetails(data: LicenseConsumption) {
+    return this.httpClient.put(`${this.API_URL}/${data.consumptionId}`, data);
+  }
+  /**
+   * delete License Usage details
+   * @param data: LicenseConsumption 
+   * @returns: Observable 
+   */
+  public deleteLicenseConsumptionDetails(consumptionId: string) {
+    return this.httpClient.delete(`${this.API_URL}/${consumptionId}`);
+  }
+
+  /**
+   * set the header for the request
+   * @returns: HttpHeaders 
+   */
+  public getHeaders() {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return headers;
+  }
+}
