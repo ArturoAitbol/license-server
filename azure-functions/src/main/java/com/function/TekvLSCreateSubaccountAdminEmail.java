2a6a41002a2249b6704cf595f298f1326df1534e
Rodrigo Vasquez <rodrigo.vasquez@aitbol.com>
2022-05-26 18:03:14 -0400

    Add POST and DELETE methods for customer_admin and subaccount_admin entities.

---
+++ azure-functions/src/main/java/com/function/TekvLSCreateSubaccountAdminEmail.java
@@ -0,0 +1,102 @@
+package com.function;
+
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+
+public class TekvLSCreateSubaccountAdminEmail {
+    private final String dbConnectionUrl = "jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVER") + "/licenses?ssl=true&sslmode=require"
+            + "&user=" + System.getenv("POSTGRESQL_USER")
+            + "&password=" + System.getenv("POSTGRESQL_PWD");
+
+    @FunctionName("TekvLSCreateSubaccountAdminEmail")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.POST},
+                    authLevel = AuthorizationLevel.ANONYMOUS,
+                    route = "subaccountAdminEmails")
+            HttpRequestMessage<Optional<CreateSubaccountAdminRequest>> request,
+            final ExecutionContext context) {
+
+        context.getLogger().info("Entering TekvLSCreateSubaccountAdminEmail Azure function");
+        context.getLogger().info("Request body: " + request);
+
+        if (!request.getBody().isPresent()) {
+            context.getLogger().info("error: request body is empty.");
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(new JSONObject("{\"error\": \"error: request body is empty.\"}")).build();
+        }
+
+        CreateSubaccountAdminRequest createSubaccountAdminRequest = request.getBody().get();
+
+        if (createSubaccountAdminRequest.getAdminEmail() == null) {
+            context.getLogger().info("error: Missing adminEmail parameter.");
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(new JSONObject("{\"error\": \"Missing mandatory parameter adminEmail.\"}")).build();
+        }
+        if (createSubaccountAdminRequest.getSubaccountId() == null) {
+            context.getLogger().info("error: Missing subaccountId parameter.");
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(new JSONObject("{\"error\": \"Missing mandatory parameter subaccountId.\"}")).build();
+        }
+
+        final String sql = "INSERT INTO subaccount_admin (subaccount_admin_email, subaccount_id) VALUES ('" + createSubaccountAdminRequest.getAdminEmail() + "', '" +
+                createSubaccountAdminRequest.getSubaccountId() + "');";
+
+        try (Connection connection = DriverManager.getConnection(dbConnectionUrl);
+             Statement statement = connection.createStatement()) {
+
+            context.getLogger().info("Successfully connected to:" + dbConnectionUrl);
+
+            context.getLogger().info("Execute SQL statement: " + sql);
+            statement.executeUpdate(sql);
+            context.getLogger().info("License usage inserted successfully.");
+
+            return request.createResponseBuilder(HttpStatus.OK).build();
+
+        } catch (SQLException e) {
+            context.getLogger().info("SQL exception: " + e.getMessage());
+            JSONObject json = new JSONObject();
+            json.put("error", e.getMessage());
+            return request.createResponseBuilder(HttpStatus.OK).body(json.toString()).build();
+        } catch (Exception e) {
+            context.getLogger().info("Caught exception: " + e.getMessage());
+            JSONObject json = new JSONObject();
+            json.put("error", e.getMessage());
+            return request.createResponseBuilder(HttpStatus.OK).body(json.toString()).build();
+        }
+
+    }
+
+    public static class CreateSubaccountAdminRequest {
+        private final String adminEmail;
+        private final String subaccountId;
+
+        public CreateSubaccountAdminRequest(String adminEmail, String subaccountId) {
+            this.adminEmail = adminEmail;
+            this.subaccountId = subaccountId;
+        }
+
+        public String getAdminEmail() {
+            return adminEmail;
+        }
+
+        public String getSubaccountId() {
+            return subaccountId;
+        }
+
+        @Override
+        public String toString() {
+            return "CreateSubaccountAdminRequest{" +
+                    "adminEmail='" + adminEmail + '\'' +
+                    ", customerId='" + subaccountId + '\'' +
+                    '}';
+        }
+    }
+}
