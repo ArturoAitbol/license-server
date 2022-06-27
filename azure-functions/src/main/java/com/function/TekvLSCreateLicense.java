d9979bd8ac4b4c2662d9ef4ff363fe6541cdb4f2
VictorTorricoS <vtorrico@aitbol.com>
2022-06-27 12:53:33 -0400

    fixed issue with renewal date lower than today

--- azure-functions/src/main/java/com/function/TekvLSCreateLicense.java
+++ azure-functions/src/main/java/com/function/TekvLSCreateLicense.java
@@ -11,6 +11,8 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import java.sql.*;
 import java.util.Optional;
+import java.time.LocalDate; 
+import java.time.format.DateTimeFormatter;
 
 import org.json.JSONObject;
 
@@ -82,8 +84,17 @@ public class TekvLSCreateLicense
 				return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(json.toString()).build();
 			}
 		}
-		sqlPart1 += "status" + ",";
-		sqlPart2 += "'" + "Active" + "',"; 
+		LocalDate currentDate = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd");
+		String actualDate = currentDate.format(formatter);
+		String renewalDate = jobj.getString("renewalDate");
+		if ((renewalDate.compareTo(actualDate)) < 0 ){
+			sqlPart1 += "status" + ",";
+			sqlPart2 += "'" + "Expired" + "',"; 
+		}else{
+			sqlPart1 += "status" + ",";
+			sqlPart2 += "'" + "Active" + "',"; 
+		}
 		// Remove the comma after the last parameter and build the SQL statement
 		sqlPart1 = sqlPart1.substring(0, sqlPart1.length() - 1);
 		sqlPart2 = sqlPart2.substring(0, sqlPart2.length() - 1);
