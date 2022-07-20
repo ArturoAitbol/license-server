046aa6322613618149aa4d5c0a282a908d8ae049
Arturo-AB <arturo.amoroso@atombeamtech.com>
2022-07-20 09:31:57 -0400

    Modify .bat script to restore the local database

--- azure-functions/src/test/resources/Script.bat
+++ azure-functions/src/test/resources/Script.bat
@@ -3,19 +3,25 @@
 echo "DB Restore"
 echo "============================="
 postgres "-V"
-IF "%?%" "-ne" "0" (
-  echo "Postgres not installed, please install it manually and then run this script again."
-  exit "-1"
-) ELSE (
-  echo "*******Local Restore*******"
-@REM   echo "Type your Postgres username"
-@REM   read "username"
-  psql -d postgres -U root < db_scripts/latest_license_server.sql -q
-  IF "%?%" "-ne" "0" (
-    echo "Error executing DB restore"
-    exit "-1"
-  ) ELSE (
-    echo "DB Restore finished!"
-    exit "0"
-  )
-)
+
+if %ERRORLEVEL% NEQ 0 (
+  echo Postgres not installed, please install it manually and then run this script again.
+  goto :error)
+
+echo "*******Local Restore*******"
+set /p username="Type your Postgres username:"
+set /p password="Type the password of %username% user:"
+
+REM psql -d postgres -U %username% < db_scripts/latest_license_server.sql -q
+psql "dbname=postgres user=%username% password=%password%" < db_scripts/latest_license_server.sql -q
+
+  IF %ERRORLEVEL% NEQ 0 (
+    echo Error executing DB restore
+    goto :error) ELSE (
+    echo DB Restore finished!
+    exit /b %errorlevel%
+    )
+
+:error
+echo Failed with error: %errorlevel%.
+exit /b %errorlevel%
