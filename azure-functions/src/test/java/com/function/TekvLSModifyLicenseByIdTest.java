b514a0acf5bfff85aeb2e37e8139a0658c69653a
ariel <ariel_arc.95@hotmail.com>
2022-07-21 14:58:49 -0400

    Add Unit test for APIs: Create, Delete, Get and Modify Licenses

---
+++ azure-functions/src/test/java/com/function/TekvLSModifyLicenseByIdTest.java
@@ -0,0 +1,238 @@
+package com.function;
+
+import com.function.auth.RoleAuthHandler;
+import com.function.util.Config;
+import com.function.util.TekvLSTest;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.HttpStatusType;
+import org.json.JSONObject;
+import org.junit.jupiter.api.*;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+class TekvLSModifyLicenseByIdTest extends TekvLSTest {
+
+    private final TekvLSModifyLicenseById tekvLSModifyLicenseById = new TekvLSModifyLicenseById();
+    private final TekvLSCreateLicense tekvLSCreateLicense = new TekvLSCreateLicense();
+    private final TekvLSDeleteLicenseById tekvLSDeleteLicenseById = new TekvLSDeleteLicenseById();
+    private final String licenseId = "31d82e5c-b911-460d-edbe-6860f8464233";
+
+    @BeforeEach
+    void setup() {
+        this.initTestParameters();
+        this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("fullAdmin"));
+        String bodyRequest = "{'subaccountId': '04dfda26-98f4-42e5-889a-3edccf4b799c'," +
+                "'startDate': '2023-06-01T00:00:00.000Z'," +
+                "'packageType': 'Basic'," +
+                "'renewalDate': '2023-06-10T04:00:00.000Z'," +
+                "'tokensPurchased': '55'," +
+                "'deviceLimit': '5000'," +
+                "'licenseId':'"+licenseId+"'," +
+                "'status': 'Active'}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        HttpResponseMessage response = tekvLSCreateLicense.run(this.request,this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+    }
+
+    @AfterEach
+    void tearDown(){
+        this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("fullAdmin"));
+        HttpResponseMessage response = tekvLSDeleteLicenseById.run(this.request,this.licenseId, this.context);
+        this.context.getLogger().info(response.getStatus().toString());
+
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+    }
+
+    @Tag("acceptance")
+    @Test
+    public void modifyLicenseTest(){
+        //Given
+        String bodyRequest = "{'packageType': 'Small'," +
+                "'tokensPurchased': '150'," +
+                "'deviceLimit': '5000'}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId,this.context);
+        this.context.getLogger().info(response.getStatus().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+    }
+
+    @Tag("acceptance")
+    @Test
+    public void modifyLicenseEmptyBodyTest(){
+        //Given
+        String bodyRequest = "{}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId,this.context);
+        this.context.getLogger().info(response.getStatus().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+    }
+
+    @Test
+    public void modifyLicenseNoBodyTest(){
+        //Given
+        String bodyRequest = "";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId,this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+        assertTrue(jsonBody.has("error"));
+
+        String actualResponse = jsonBody.getString("error");
+        String expectedResponse = "error: request body is empty.";
+        assertEquals(expectedResponse,actualResponse,"Response doesn't match with: ".concat(expectedResponse));
+
+    }
+
+    @Test
+    public void modifyLicenseInvalidBodyTest(){
+        //Given - Arrange
+        String bodyRequest = "invalid-body";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        //When - Action
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId,this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+        assertTrue(jsonBody.has("error"));
+
+        String expectedResponse = "A JSONObject text must begin with '{' at 1 [character 2 line 1]";
+        String actualResponse = jsonBody.getString("error");
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Tag("Security")
+    @Test
+    public void modifyLicenseNoTokenTest(){
+        //Given
+        this.headers.remove("authorization");
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId, this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expectedStatus = HttpStatus.UNAUTHORIZED;
+        assertEquals(expectedStatus, actualStatus,"HTTP status doesn't match with: ".concat(expectedStatus.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+        assertTrue(jsonBody.has("error"));
+
+        String actualResponse = jsonBody.getString("error");
+        String expectedResponse = RoleAuthHandler.MESSAGE_FOR_UNAUTHORIZED;
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Tag("Security")
+    @Test
+    public void modifyLicenseInvalidRoleTest(){
+        //Given
+        this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("test"));
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId, this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expectedStatus = HttpStatus.FORBIDDEN;
+        assertEquals(expectedStatus, actualStatus,"HTTP status doesn't match with: ".concat(expectedStatus.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+        assertTrue(jsonBody.has("error"));
+
+        String actualResponse = jsonBody.getString("error");
+        String expectedResponse = RoleAuthHandler.MESSAGE_FOR_FORBIDDEN;
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Test
+    public void modifyLicenseInvalidSQLTest(){
+        //Given
+        String invalidId = "invalid-id";
+        String bodyRequest = "{'packageType': 'Small'," +
+                "'tokensPurchased': '150'," +
+                "'deviceLimit': '5000'}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,invalidId,this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.INTERNAL_SERVER_ERROR;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+    }
+
+    @Test
+    public void modifyLicenseGenericExceptionTest(){
+        //Given
+        String bodyRequest = "{'packageType': 'Small'," +
+                "'tokensPurchased': '150'," +
+                "'deviceLimit': '5000'}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+        doThrow(new RuntimeException("Error message")).when(this.request).createResponseBuilder(HttpStatus.OK);
+
+        //When
+        HttpResponseMessage response = tekvLSModifyLicenseById.run(this.request,this.licenseId, this.context);
+        this.context.getLogger().info(response.getBody().toString());
+
+        //Then
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        assertEquals(expected, actualStatus,"HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+        assertTrue(jsonBody.has("error"));
+
+        String actualResponse = jsonBody.getString("error");
+        String expectedResponse = "Error message";
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+
+        this.initTestParameters();
+    }
+}
