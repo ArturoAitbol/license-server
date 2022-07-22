65ff31cc459bf88819ec809fb087fac91a52ff62
Rodrigo Vasquez <rodrigo.vasquez@aitbol.com>
2022-07-22 14:06:13 -0400

    Add unit tests to subaccount APIs

---
+++ azure-functions/src/test/java/com/function/TekvLSDeleteSubaccountByIdTest.java
@@ -0,0 +1,161 @@
+package com.function;
+
+import com.function.auth.RoleAuthHandler;
+import com.function.util.Config;
+import com.function.util.TekvLSTest;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.HttpStatusType;
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+
+class TekvLSDeleteSubaccountByIdTest extends TekvLSTest {
+
+    TekvLSDeleteSubaccountById deleteSubaccountApi = new TekvLSDeleteSubaccountById();
+
+    private final TekvLSCreateSubaccount createSubaccountApi = new TekvLSCreateSubaccount();
+    private String subaccountId = "EMPTY";
+
+    @BeforeEach
+    public void setup() {
+        this.initTestParameters();
+        this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("fullAdmin"));
+        String name = "unitTest" + LocalDateTime.now();
+        String bodyRequest = "{\n" +
+                "    \"subaccountName\": \"" + name + "\",\n" +
+                "    \"customerId\": 740162ed-3abe-4f89-89ef-452e3c0787e2,\n" +
+                "    \"subaccountAdminEmail\": \"" + name + "@test.com\"\n" +
+                "}";
+        doReturn(Optional.of(bodyRequest)).when(request).getBody();
+        HttpResponseMessage response = this.createSubaccountApi.run(this.request, context);
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+        this.subaccountId = new JSONObject(response.getBody().toString()).getString("id");
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (!this.subaccountId.equals("EMPTY")) {
+            this.initTestParameters();
+            this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("fullAdmin"));
+            HttpResponseMessage response = deleteSubaccountApi.run(this.request, this.subaccountId, this.context);
+            this.context.getLogger().info(response.getStatus().toString());
+            this.subaccountId = "EMPTY";
+
+            HttpStatusType actualStatus = response.getStatus();
+            HttpStatus expected = HttpStatus.OK;
+            assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+        }
+    }
+
+    @Test
+    public void deleteSubaccountTest() {
+        //When - Action
+        HttpResponseMessage response = deleteSubaccountApi.run(this.request, this.subaccountId, this.context);
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.OK;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+
+    }
+
+    @Test
+    public void invalidIdTest() {
+        //When - Action
+        HttpResponseMessage response = deleteSubaccountApi.run(this.request, "0", this.context);
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.INTERNAL_SERVER_ERROR;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+
+        assertTrue(jsonBody.has("error"));
+
+        String expectedResponse = "ERROR: invalid input syntax for type uuid: \"0\"\n" + "  Position: 33";
+        String actualResponse = jsonBody.getString("error");
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Test
+    public void genericExceptionTest() {
+        //Given - Arrange
+        Mockito.doThrow(new RuntimeException("Generic error")).when(request).createResponseBuilder(HttpStatus.OK);
+
+        //When - Action
+        HttpResponseMessage response = deleteSubaccountApi.run(this.request, this.subaccountId, this.context);
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.BAD_REQUEST;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+
+        assertTrue(jsonBody.has("error"));
+
+        String expectedResponse = "Generic error";
+        String actualResponse = jsonBody.getString("error");
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Test
+    public void unauthorizedTest() {
+        //Given - Arrange
+        this.headers.remove("authorization");
+        //When - Action
+        HttpResponseMessage response = deleteSubaccountApi.run(this.request, this.subaccountId, this.context);
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.UNAUTHORIZED;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+
+        assertTrue(jsonBody.has("error"));
+
+        String expectedResponse = RoleAuthHandler.MESSAGE_FOR_UNAUTHORIZED;
+        String actualResponse = jsonBody.getString("error");
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+
+    @Test
+    public void forbiddenTest() {
+        //Given - Arrange
+        this.headers.remove("authorization");
+        this.headers.put("authorization", "Bearer " + Config.getInstance().getToken("devicesAdmin"));
+        //When - Action
+        HttpResponseMessage response = deleteSubaccountApi.run(this.request, this.subaccountId, this.context);
+
+        //Then - Assert
+        HttpStatusType actualStatus = response.getStatus();
+        HttpStatus expected = HttpStatus.FORBIDDEN;
+        assertEquals(expected, actualStatus, "HTTP status doesn't match with: ".concat(expected.toString()));
+
+        String body = (String) response.getBody();
+        JSONObject jsonBody = new JSONObject(body);
+
+        assertTrue(jsonBody.has("error"));
+
+        String expectedResponse = RoleAuthHandler.MESSAGE_FOR_FORBIDDEN;
+        String actualResponse = jsonBody.getString("error");
+        assertEquals(expectedResponse, actualResponse, "Response doesn't match with: ".concat(expectedResponse));
+    }
+}
