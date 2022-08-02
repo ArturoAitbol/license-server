8100a3cc5879418a73d6d01ea9e25490717c3314
VictorTorricoS <vtorrico@aitbol.com>
2022-08-02 13:48:46 -0400

    subaccount id problem

--- ui/src/app/modules/customer/projects/projects.component.spec.ts
+++ ui/src/app/modules/customer/projects/projects.component.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatDialog, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBarModule, MatSnackBarRef } from "@angular/material/snack-bar";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Router } from "@angular/router";
 import { MsalService } from "@azure/msal-angular";
@@ -9,141 +9,58 @@ import { Observable } from "rxjs";
 import { DataTableComponent } from "src/app/generics/data-table/data-table.component";
 import { CustomerService } from "src/app/services/customer.service";
 import { DialogService } from "src/app/services/dialog.service";
+import { ProjectService } from "src/app/services/project.service";
+import { MatDialogMock } from "src/test/mock/components/mat-dialog.mock";
+import { CurrentCustomerServiceMock } from "src/test/mock/services/current-customer-service.mock";
+import { CustomerServiceMock } from "src/test/mock/services/customer-service.mock";
+import { MsalServiceMock } from "src/test/mock/services/msal-service.mock";
+import { ProjectServiceMock } from "src/test/mock/services/project-service.mock";
 import { SharedModule } from "../../shared/shared.module";
 import { AddProjectComponent } from "./add-project/add-project.component";
 import { ModifyProjectComponent } from "./modify-project/modify-project.component";
 import { ProjectsComponent } from "./projects.component";
 
 let projectsComponentTestInstance: ProjectsComponent;
-
 let fixture: ComponentFixture<ProjectsComponent>;
 
-let projectServiceMock = {
-    getProjectList: () => {
-        return new Observable((observer) => {
-            observer.next(
-                {
-                    'projects':[
-                        {
-                            id: '88888888-8888-8888-8888-888888888888',
-                            name: 'Project-Test1',
-                            number: 'test-code',
-                            subaccountId: '99999999-9999-9999-9999-999999999999',
-                            openDate: '2022-07-26 05:00:00',
-                            closeDate: '2022-08-29 05:00:00',
-                            status: 'Open'
-                        },
-                        {
-                            id: '22222222-2222-2222-2222-22222222222',
-                            name: 'Project-Test2',
-                            number: 'test-code2',
-                            subaccountId: '99999999-9999-9999-9999-999999999999',
-                            openDate: '2022-07-25 05:00:00',
-                            closeDate: '2022-08-30 05:00:00',
-                            status: 'Open'
-                        }
-                    ]
-                }
-            );
-            return {
-                unsubscribe() {
-                    console.log('unsubscribed from projectService Observable')
-                }
-            };
-        });
-    }
-};
+const RouterMock = {
+    navigate: (commands: string[]) => {}
+}
 
-let customerServiceMock = {
-    getSelectedCustomer: () => {
-        return new Observable( (observer) => {
-            observer.next(
-            {
-                    "customer": [
-                        {
-                            id: '11111111-1111-1111-1111-11111111111',
-                            name: 'TestCustomerP',
-                            status: 'Active',
-                            customerType: 'MSP',
-                            subaccountId: '99999999-9999-9999-9999-999999999999',
-                            subaccountName: 'testSubP',
-                            testCustomer: true
-                        }
-                    ]
-                }
-            );
-            return {
-                unsubscribe() {
-                    console.log('unsubscribed from getCustomerList Observable');
-                }
-            };
-        });
-    }
-};
-
-beforeEach(() => {
+const beforeEachFunction = () => {
     TestBed.configureTestingModule({
         declarations: [ ProjectsComponent, DataTableComponent, ModifyProjectComponent, AddProjectComponent ],
-        imports: [BrowserAnimationsModule, MatDialogModule, MatSnackBarModule, SharedModule],
+        imports: [BrowserAnimationsModule, MatSnackBarModule, SharedModule],
         providers:[
             {
                 provide: Router,
+                useValue: RouterMock
+            },
+            {
+                provide: MatDialog,
+                useValue: MatDialogMock
+            },
+            {
+                provide: MatSnackBarRef,
                 useValue: {}
             },
             {
-                provide: MatDialogRef,
-                useValue: {}
-            },
-            {
-                provide: ProjectsComponent,
-                useValue: projectServiceMock
+                provide: ProjectService,
+                useValue: ProjectServiceMock
             },
             {
                 provide: DialogService,
                 useValue: () => {
-                    console.log('DialogService Mock')
                     return{};
                 }
             },
             {
                 provide: CustomerService,
-                useValue: customerServiceMock
+                useValue: CurrentCustomerServiceMock
             },
             {
                 provide: MsalService,
-                useValue: {
-                    instance: {
-                        getActiveAccount: () =>{
-                            return {
-                                'homeAccountId': '4bd8345a-b441-4791-91ff-af23e4b02e02.e3a46007-31cb-4529-b8cc-1e59b97ebdbd',
-                                'environment': 'login.windows.net',
-                                'tenantId': 'e3a46007-31cb-4529-b8cc-1e59b97ebdbd',
-                                'username': 'lvelarde@tekvizionlabs.com',
-                                'localAccountId': '4bd8345a-b441-4791-91ff-af23e4b02e02',
-                                'name': 'Leonardo Velarde',
-                                idTokenClaims: {
-                                    'aud': 'e643fc9d-b127-4883-8b80-2927df90e275',
-                                    'iss': 'https://login.microsoftonline.com/e3a46007-31cb-4529-b8cc-1e59b97ebdbd/v2.0',
-                                    'iat': 1657823518,
-                                    'nbf': 1657823518,
-                                    'exp': 1657827418,
-                                    'name': 'Leonardo Velarde',
-                                    'nonce': '41279ac2-8254-4f82-a11b-38fd27248c57',
-                                    'oid': '4bd8345a-b441-4791-91ff-af23e4b02e02',
-                                    'preferred_username': 'lvelarde@tekvizionlabs.com',
-                                    'rh': '0.ARMAB2Ck48sxKUW4zB5ZuX69vZ38Q-YnsYNIi4ApJ9-Q4nUTAEs.',
-                                    roles: [
-                                        'tekvizion.FullAdmin'
-                                    ],
-                                    'sub': 'q_oqvIR8gLozdXv-rtEYPNfPc0y4AfLlR_LiKUxZSy0',
-                                    'tid': 'e3a46007-31cb-4529-b8cc-1e59b97ebdbd',
-                                    'uti': 'GwgRbk67AECociiD7H0SAA',
-                                    'ver': '2.0'
-                                }
-                            };
-                        }
-                    }
-                }
+                useValue: MsalServiceMock
             },
             {
                 provide: HttpClient,
@@ -155,25 +72,29 @@ beforeEach(() => {
     projectsComponentTestInstance = fixture.componentInstance;
     projectsComponentTestInstance.ngOnInit();
     spyOn(console, 'log').and.callThrough;
-    spyOn(projectServiceMock, 'getProjectList').and.callThrough;
-    spyOn(customerServiceMock, 'getSelectedCustomer').and.callThrough;
-});
+    spyOn(CurrentCustomerServiceMock,'getSelectedCustomer' ).and.callThrough;
+    spyOn(ProjectServiceMock, 'setSelectedSubAccount').and.callThrough;
+    spyOn(ProjectServiceMock, 'getProjectDetailsBySubAccount').and.callThrough;
+    
+};
 
 describe('UI verrification test', () => {
+    beforeEach(beforeEachFunction);
     it('should display essential UI and components', () =>{
         fixture.detectChanges();
-        let h2 = fixture.nativeElement.querySelector('#page-title');
-        let addProjectButton = fixture.nativeElement.querySelector('#add-new-project-button');
+        const h2 = fixture.nativeElement.querySelector('#page-title');
+        const addProjectButton = fixture.nativeElement.querySelector('#add-new-project-button');
         expect(h2.textContent).toBe('Project Summary');
-        expect(addProjectButton.textContent).toBe('Add New Projects')
+        expect(addProjectButton.textContent).toBe('Add New Projects');
     });
 
-    it('should load correct data columns for the table', () =>{
-        let projectCodeColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[0];
-        let projectNameColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[1];
-        let projectStatusColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[2];
-        let projectStartDateColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[3];
-        let projectCloseDateColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[4];
+    it('should load correct data columns for the table', () => {
+        fixture.detectChanges();
+        const projectCodeColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[0];
+        const projectNameColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[1];
+        const projectStatusColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[2];
+        const projectStartDateColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[3];
+        const projectCloseDateColumn = fixture.nativeElement.querySelectorAll('.mat-sort-header-content')[4];
         expect(projectCodeColumn.innerText).toBe('Project Code');
         expect(projectNameColumn.innerText).toBe('Project Name');
         expect(projectStatusColumn.innerText).toBe('Status');
@@ -183,17 +104,60 @@ describe('UI verrification test', () => {
 });
 
 describe('Data collection and parsin tests', () => {
+    beforeEach(beforeEachFunction);
     it('should make a call to get project list after initializing', () => {
         fixture.detectChanges();
-        expect(projectServiceMock.getProjectList).toHaveBeenCalled();
-        expect(customerServiceMock.getSelectedCustomer).toHaveBeenCalled();
+        expect(CurrentCustomerServiceMock.getSelectedCustomer).toHaveBeenCalled();
+        expect(ProjectServiceMock.setSelectedSubAccount).toHaveBeenCalled();
+        expect(ProjectServiceMock.getProjectDetailsBySubAccount).toHaveBeenCalled();
     });
+}); 
+
+
+describe('Dialog calls and ionteractions', () => {
+    beforeEach(beforeEachFunction);
+
+    it('should execute rowAction() with expected data given set arguments', () => {
+        const selectedTestData = { selectedRow: { testProperty: 'testData'}, selectedOption: 'selectedTestOption', selectedIndex: 'selectedTestItem' };
+        spyOn(projectsComponentTestInstance, 'openDialog');
+        spyOn(projectsComponentTestInstance, 'confirmCloseDialog');
+        spyOn(projectsComponentTestInstance, 'confirmDeleteDialog');
+        spyOn(projectsComponentTestInstance, 'openConsumptionView');
+        
+        selectedTestData.selectedOption = projectsComponentTestInstance.MODIFY_PROJECT;
+        projectsComponentTestInstance.rowAction(selectedTestData);
+        expect(projectsComponentTestInstance.openDialog).toHaveBeenCalledWith(ModifyProjectComponent, selectedTestData.selectedRow);
+
+        selectedTestData.selectedOption = projectsComponentTestInstance.CLOSE_PROJECT;
+        projectsComponentTestInstance.rowAction(selectedTestData);
+        expect(projectsComponentTestInstance.confirmCloseDialog).toHaveBeenCalledWith(selectedTestData.selectedIndex);
+
+        selectedTestData.selectedOption = projectsComponentTestInstance.CLOSE_PROJECT;
+        projectsComponentTestInstance.rowAction(selectedTestData);
+        expect(projectsComponentTestInstance.confirmCloseDialog).toHaveBeenCalledWith(selectedTestData.selectedIndex);
+
+        selectedTestData.selectedOption = projectsComponentTestInstance.DELETE_PROJECT;
+        projectsComponentTestInstance.rowAction(selectedTestData);
+        expect(projectsComponentTestInstance.confirmDeleteDialog).toHaveBeenCalledWith(selectedTestData.selectedIndex);
+
+        selectedTestData.selectedOption = projectsComponentTestInstance.VIEW_CONSUMPTION;
+        projectsComponentTestInstance.rowAction(selectedTestData);
+        expect(projectsComponentTestInstance.openConsumptionView).toHaveBeenCalledWith(selectedTestData.selectedIndex);
+    })
 });
 
-describe('Navigation', () => {
-    it('should make a call to get project list after initializing', () => {
-        fixture.detectChanges();
-        expect(projectServiceMock.getProjectList).toHaveBeenCalled();
-        expect(customerServiceMock.getSelectedCustomer).toHaveBeenCalled();
+describe('navigate', () =>{
+    beforeEach(beforeEachFunction);
+
+    it('should navigate to license consumption after calling openConsumptionView()', () => {
+        spyOn(RouterMock, 'navigate');
+        projectsComponentTestInstance.openConsumptionView({});
+        expect(RouterMock.navigate).toHaveBeenCalledWith(['/customer/consumption']);
+    });
+
+    it('should navigate to dashboard after calling goToDashboard()', () => {
+        spyOn(RouterMock, 'navigate');
+        projectsComponentTestInstance.goToDashboard();
+        expect(RouterMock.navigate).toHaveBeenCalledWith(['/dashboard']);
     });
 });
---
+++ ui/src/test/mock/services/project-service.mock.ts
@@ -0,0 +1,51 @@
+import {Observable} from 'rxjs';
+
+const PROJECT_LIST = {
+    projects: [
+        {
+            id: '459cf3ca-7365-47a1-8d9b-1abee381545c',
+            name: 'Project-Test1',
+            code: 'test-code',
+            subaccountId: 'eea5f3b8-37eb-41fe-adad-5f94da124a5a',
+            openDate: '2022-07-26 05:00:00',
+            closeDate: '2022-08-29 05:00:00',
+            status: 'Open'
+        },
+        {
+            id: '6eb1f15b-168d-4ef0-adb1-fec73b65af25',
+            name: 'Project-Test2',
+            code: 'test-code2',
+            subaccountId: 'eea5f3b8-37eb-41fe-adad-5f94da124a5a',
+            openDate: '2022-07-25 05:00:00',
+            closeDate: '2022-08-30 05:00:00',
+            status: 'Open'
+        },
+        {
+            id: '9fd20dca-33f0-4bd2-b484-d81dd6423626',
+            name: 'Project-Test3',
+            code: 'test-code3',
+            subaccountId: 'eea5f3b8-37eb-41fe-adad-5f94da124a5a',
+            openDate: '2022-07-25 05:00:00',
+            closeDate: '2022-08-30 05:00:00',
+            status: 'Open'
+        }
+    ]
+};
+
+export const ProjectServiceMock = {
+    customerListValue: PROJECT_LIST,
+    getProjectDetailsBySubAccount: () => {
+        return new Observable((observer) => {
+            observer.next(
+                PROJECT_LIST
+            );
+            observer.complete();
+            return {
+                unsubscribe() { }
+            };
+        });
+    },
+    setSelectedSubAccount: () => {
+
+    }
+};
