b8bc064790c836af4c36e7af01c74748736992e4
Arturo-AB <arturo.amoroso@atombeamtech.com>
2022-08-12 22:21:13 -0400

    Create, edit, and delete a subaccount

--- tests/ui/src/main/java/ui/pages/ActionMenu.java
+++ tests/ui/src/main/java/ui/pages/ActionMenu.java
@@ -4,20 +4,18 @@ import org.openqa.selenium.By;
 import ui.core.AbstractPageObject;
 
 public class ActionMenu extends AbstractPageObject {
-    AbstractPageObject pageObject;
     By messageSelector = By.cssSelector(".cdk-overlay-container snack-bar-container");
-    public ActionMenu(AbstractPageObject pageObject){
-        this.pageObject = pageObject;
-    }
 
-    public String delete(){
+    public String delete(String type){
         By deleteSelector = By.cssSelector("button#Delete");
         this.action.click(deleteSelector);
         Modal confirmModal = new Modal();
-        if (this.pageObject instanceof CustomerRow)
+        if (type.equals("customer")){
             confirmModal.reconfirmAction();
-        else
+        }
+        else{
             confirmModal.confirmAction();
+        }
         return this.action.getText(this.messageSelector);
     }
 
