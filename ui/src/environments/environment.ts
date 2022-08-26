3578aef3697e8a965166d878bdb99484ae913939
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-08-26 14:13:07 -0400

    replaced env

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  //apiEndpoint: 'https://tekvlicenseserverpocfunction.azurewebsites.net/v1.0',
   apiEndpoint: 'http://localhost:7071/v1.0',
   // Azure Active Directory Application details
+  AUTHORITY: 'https://login.microsoftonline.com/e3a46007-31cb-4529-b8cc-1e59b97ebdbd',
   TENANT_ID: 'e3a46007-31cb-4529-b8cc-1e59b97ebdbd',
   UI_CLIENT_ID: 'e643fc9d-b127-4883-8b80-2927df90e275',
   API_CLIENT_ID: 'abb49487-0434-4a82-85fa-b9be4443d158',
