5a7ea1da8821a36bc4e55acce34249bdf6253f77
Paolo Fernandez <pfernandez@aitbol.com>
2022-08-26 11:36:13 -0400

    reverting consumptionRow

--- tests/ui/src/main/java/ui/pages/ConsumptionRow.java
+++ tests/ui/src/main/java/ui/pages/ConsumptionRow.java
@@ -6,7 +6,7 @@ import ui.core.AbstractPageObject;
 public class ConsumptionRow extends AbstractPageObject {
     private final String PROJECT_NAME_XPATH;
     public ConsumptionRow(String projectName){
-        this.PROJECT_NAME_XPATH = String.format("//div[@id='detailed-consumption-table' and td[@title='%s']]", projectName);
+        this.PROJECT_NAME_XPATH = String.format("//td[@title='%s']", projectName);
     }
 
     public String getColumnValue(String column){
@@ -22,6 +22,7 @@ public class ConsumptionRow extends AbstractPageObject {
 
     public ActionMenu openActionMenu(){
         By actionMenuSelector = By.xpath(this.PROJECT_NAME_XPATH + "/following-sibling::td[@id='more_vert']/button");
+        // By actionMenuSelector = By.xpath("//td[@id='more_vert']/button");
         this.action.click(actionMenuSelector);
         return new ActionMenu();
     }
