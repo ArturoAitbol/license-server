379d9fed51aee33d3356d3dcb2f8d197f4b8c4d1
Paolo Fernandez <pfernandez@aitbol.com>
2022-09-07 19:32:29 -0400

    adding example of msalService send as parameter for the user feature toggle

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -48,7 +48,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         // example for a feature toggle in typescript logic code
-        if (FeatureToggleHelper.isFeatureEnabled("testFeature1"))
+        if (FeatureToggleHelper.isFeatureEnabled("testFeature1", this.msalService))
             console.log("Feature toggle 'testFeature1' enabled");
         if (!this.isLoggedIn()) {
             this.router.navigate(['/login']);
