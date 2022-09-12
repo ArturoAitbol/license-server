73101db3767119fd69ceaaf3f9d5abed367a2f51
Kaushik Nalla <knalla@tekvizion.com>
2022-09-12 12:11:15 +0530

    Revert "Revert "Enhanced  list subaccount api""
    This reverts commit c23dfaab9a7c5be37cf88b02a8ded61633542bf0.

--- azure-functions/src/test/java/com/function/util/Config.java
+++ azure-functions/src/test/java/com/function/util/Config.java
@@ -104,6 +104,12 @@ public class Config {
                 username = "subaccountAdmin_user";
                 password = "subaccountAdmin_password";
                 break;
+            case "subaccountStakeholder":
+                roleId = "subaccountStakeholderId";
+                roleSecret = "subaccountStakeholderSecret";
+                username = "subaccountStakeholder_user";
+                password = "subaccountStakeholder_password";
+                break;
             case "crm":
                 roleId = "crmId";
                 roleSecret = "crmSecret";
