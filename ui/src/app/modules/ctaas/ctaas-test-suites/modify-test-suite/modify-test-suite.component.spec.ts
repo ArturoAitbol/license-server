a3a2c871f19972a34e31aebef41251d034244b30
Oscar Gonzalez <oscar_sgc@icloud.com>
2022-09-23 16:21:16 -0400

    Comment on UI Test

--- ui/src/app/modules/ctaas/ctaas-test-suites/modify-test-suite/modify-test-suite.component.spec.ts
+++ ui/src/app/modules/ctaas/ctaas-test-suites/modify-test-suite/modify-test-suite.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModifyTestSuiteComponent } from './modify-test-suite.component';
+// import { ModifyTestSuiteComponent } from './modify-test-suite.component';
 
-describe('ModifyTestSuiteComponent', () => {
-  let component: ModifyTestSuiteComponent;
-  let fixture: ComponentFixture<ModifyTestSuiteComponent>;
+// describe('ModifyTestSuiteComponent', () => {
+//   let component: ModifyTestSuiteComponent;
+//   let fixture: ComponentFixture<ModifyTestSuiteComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ModifyTestSuiteComponent ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ ModifyTestSuiteComponent ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ModifyTestSuiteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(ModifyTestSuiteComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
