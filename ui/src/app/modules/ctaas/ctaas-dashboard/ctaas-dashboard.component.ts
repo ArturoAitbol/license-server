e3607812538b88492c4887ed16297e3094b24b03
suibbarao <113896315+suibbarao@users.noreply.github.com>
2022-10-10 13:55:45 +0530

    removed elementref form imports

--- ui/src/app/modules/ctaas/ctaas-dashboard/ctaas-dashboard.component.ts
+++ ui/src/app/modules/ctaas/ctaas-dashboard/ctaas-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OnboardWizardComponent } from '../onboard-wizard/onboard-wizard.component';
 import { MsalService } from '@azure/msal-angular';
