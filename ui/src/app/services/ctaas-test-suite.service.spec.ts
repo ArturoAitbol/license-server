817351a29edf2229de1efd573b8ef4a58fbbc35c
samvs6 <samuel-vs6@hotmail.com>
2022-10-20 11:52:16 -0400

    coverage improvement

--- ui/src/app/services/ctaas-test-suite.service.spec.ts
+++ ui/src/app/services/ctaas-test-suite.service.spec.ts
@@ -3,28 +3,30 @@ import {CtaasTestSuiteService} from './ctaas-test-suite.service';
 import {environment} from '../../environments/environment';
 import {TestSuite} from "../model/test-suite.model";
 import {TestSuitesMock} from '../../test/mock/services/ctaas-test-suites.service.mock';
+import { map } from 'rxjs/operators'
 
 let httpClientSpy: jasmine.SpyObj<HttpClient>;
 let ctaasTestSuiteService: CtaasTestSuiteService;
 const headers = new HttpHeaders();
 headers.append('Content-Type', 'application/json');
 
-describe('Customer service http requests test', () => {
+describe('CTaaS Test Suite service http requests test', () => {
     beforeEach(async () => {
         httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
         ctaasTestSuiteService = new CtaasTestSuiteService(httpClientSpy);
     });
 
-    // it('should make the proper calls on getTestSuitesBySubAccount()', (done: DoneFn) => {
-    //     let params = new HttpParams();
-    //     params = params.append('subaccountId', 'fbb2d912-b202-432d-8c07-dce0dad51f7f');
-    //     httpClientSpy.get.and.returnValue(TestSuitesMock.getTestSuitesBySubAccount());
-    //     ctaasTestSuiteService.getTestSuitesBySubAccount('fbb2d912-b202-432d-8c07-dce0dad51f7f').subscribe({
-    //         next: () => { done(); },
-    //         error: done.fail
-    //     });
-    //     expect(httpClientSpy.get).toHaveBeenCalledWith(environment.apiEndpoint + '/ctaasTestSuites', { headers, params});
-    // });
+    it('should make the proper calls on getTestSuitesBySubAccount()', (done: DoneFn) => {
+        let params = new HttpParams();
+        params = params.set('subaccountId', 'fbb2d912-b202-432d-8c07-dce0dad51f7f');
+        httpClientSpy.get.and.returnValue(TestSuitesMock.getTestSuitesBySubAccount());
+        ctaasTestSuiteService.getTestSuitesBySubAccount('fbb2d912-b202-432d-8c07-dce0dad51f7f').subscribe({
+            next: () => { done(); },
+            error: done.fail
+        });
+        console.log(JSON.stringify(params));
+        expect(httpClientSpy.get).toHaveBeenCalledWith(environment.apiEndpoint + '/ctaasTestSuites', { headers, params});
+    });
 
     it('should make the proper http calls on updateTestSuite()', (done: DoneFn) => {
         const updatedTestSuite: TestSuite = {
