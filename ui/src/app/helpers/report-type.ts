2e21e2231cae5321a2e0540b35f83735ac8f4f18
sana-05 <112489748+sana-05@users.noreply.github.com>
2022-12-29 16:15:19 +0530

    PESQ mock data disabled - TP360-1058

--- ui/src/app/helpers/report-type.ts
+++ ui/src/app/helpers/report-type.ts
@@ -1,7 +1,7 @@
 export enum ReportType {
     DAILY_FEATURE_FUNCTIONALITY = 'Daily-FeatureFunctionality',
     DAILY_CALLING_RELIABILITY = 'Daily-CallingReliability',
-    DAILY_PESQ = 'Daily-PESQ',
+    // DAILY_PESQ = 'Daily-PESQ',  // as media injection is not ready yet, hence disableing PESQ as of now
     WEEKLY_FEATURE_FUNCTIONALITY = 'Weekly-FeatureFunctionality',
-    WEEKLY_PESQ = 'Weekly-PESQ'
+    // WEEKLY_PESQ = 'Weekly-PESQ'  // as media injection is not ready yet, hence disableing PESQ as of now
 }
