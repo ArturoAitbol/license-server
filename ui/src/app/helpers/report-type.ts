27ed10cfed4a46adb8e1bbbbb65653373cb0c5a1
sana-05 <112489748+sana-05@users.noreply.github.com>
2022-12-29 16:31:44 +0530

    TP360-1058

--- ui/src/app/helpers/report-type.ts
+++ ui/src/app/helpers/report-type.ts
@@ -1,7 +1,7 @@
 export enum ReportType {
     DAILY_FEATURE_FUNCTIONALITY = 'Daily-FeatureFunctionality',
     DAILY_CALLING_RELIABILITY = 'Daily-CallingReliability',
-    // DAILY_PESQ = 'Daily-PESQ',  // as media injection is not ready yet, hence disableing PESQ as of now
+    // DAILY_PESQ = 'Daily-PESQ',  // as media injection is not ready yet, hence disabling PESQ as of now
     WEEKLY_FEATURE_FUNCTIONALITY = 'Weekly-FeatureFunctionality',
-    // WEEKLY_PESQ = 'Weekly-PESQ'  // as media injection is not ready yet, hence disableing PESQ as of now
+    // WEEKLY_PESQ = 'Weekly-PESQ'  // as media injection is not ready yet, hence disabling PESQ as of now
 }
