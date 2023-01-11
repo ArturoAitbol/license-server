11b67e6b7478340300f282fb0f86c0152192737d
VictorTorricoS <vtorrico@aitbol.com>
2023-01-11 11:32:46 -0400

    added features to the reports view

--- ui/src/app/modules/ctaas/ctaas-test-reports/ctaas-test-reports.component.ts
+++ ui/src/app/modules/ctaas/ctaas-test-reports/ctaas-test-reports.component.ts
@@ -3,8 +3,11 @@ import { FormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Sort } from '@angular/material/sort';
 import { MsalService } from '@azure/msal-angular';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { Utility } from 'src/app/helpers/utils';
 import { Note } from 'src/app/model/note.model';
+import { CustomerService } from 'src/app/services/customer.service';
 import { DialogService } from 'src/app/services/dialog.service';
 import { SnackBarService } from 'src/app/services/snack-bar.service';
 import { TestReportsService } from 'src/app/services/test-reports.service';
@@ -24,6 +27,8 @@ export class CtaasTestReportsComponent implements OnInit {
   isRequestCompleted = false;
   testReportsData: any = [];
   testReportsDataBK: any = [];
+  currentCustomer: any;
+  filteredReports: any = [];
   private readonly VIEW_REPORT = 'View Report';
   private readonly VIEW_CHART = 'View Chart';
 
@@ -32,9 +37,13 @@ export class CtaasTestReportsComponent implements OnInit {
     VIEW_CHART: this.VIEW_CHART
   }
 
+  readonly reportsTypes = ['Daily-FeatureFunctionality', 'Daily-CallingReliability'];
+
   filterForm = this.fb.group({
     typeFilterControl: ['']
   });
+
+  private unsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
     private msalService: MsalService,
@@ -42,7 +51,8 @@ export class CtaasTestReportsComponent implements OnInit {
     private snackBarService: SnackBarService,
     private dialogService: DialogService,
     private testReportsService: TestReportsService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private customerService: CustomerService
   ) { }
 
   private calculateTableHeight() {
@@ -72,18 +82,33 @@ export class CtaasTestReportsComponent implements OnInit {
 
   ngOnInit(): void {
     this.calculateTableHeight();
+    this.currentCustomer = this.customerService.getSelectedCustomer();
     this.getActionMenuOptions();
     this.initColumns();
     this.fetchTestReports();
+    this.filterForm.valueChanges.pipe(
+      debounceTime(300),
+      takeUntil(this.unsubscribe)
+    ).subscribe(value => {
+      const filter = [];
+      if(value.typeFilterControl != '' && value.typeFilterControl != undefined) filter.push(report => report.reportType === value.typeFilterControl);
+      this.isLoadingResults = true;
+      this.filteredReports = this.testReportsData.filter(report => filter.every(filter => filter(report)));
+      this.isLoadingResults = false;
+    })
   }
 
   fetchTestReports() {
     // this.isRequestCompleted = false;
     // this.isLoadingResults = true;
-    // this.testReportsService.getTestReportsList().subscribe((res: any) => {
+    // this.testReportsService.getTestReportsList(this.currentCustomer.subaccountId).subscribe((res: any) => {
     //   this.isRequestCompleted = true; 
     //   this.isLoadingResults = false;
-    //   this.testReportsData = res;
+    //   this.filteredReports = this.testReportsData;
+    //   this.testReportsDataBK = this.testReportsData = res['reports'];
+    // }, () => {
+    //   this.isLoadingResults = false;
+    //   this.isRequestCompleted = true;
     // });
 
     this.isRequestCompleted = true; 
@@ -109,11 +134,11 @@ export class CtaasTestReportsComponent implements OnInit {
         console.log(this.actionMenuOptions);
         let failedTestReportIndex = this.actionMenuOptions.indexOf('Close Note');
         if (failedTestReportIndex != -1)
-            this.actionMenuOptions.splice(failedTestReportIndex, 1);
-        // this.testReportsData = this.testReportsDataBK.filter(x => x.status === 'Failed');
+          this.actionMenuOptions.splice(failedTestReportIndex, 1);
+        this.testReportsData = this.testReportsDataBK.filter(x => x.status === 'Failed');
     } else {
-        this.getActionMenuOptions();
-        // this.testReportsData = this.testReportsDataBK.filter(x => x.status === 'Successful');
+      this.getActionMenuOptions();
+      this.testReportsData = this.testReportsDataBK.filter(x => x.status === 'Successful');
     }
   }
   
@@ -159,5 +184,10 @@ export class CtaasTestReportsComponent implements OnInit {
             this.fetchTestReports();
         }
     });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 }
