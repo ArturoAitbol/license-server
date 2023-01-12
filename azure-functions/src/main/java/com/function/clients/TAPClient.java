3f65f040e9b59156d13b9f5a1b5f3354d4999ba0
Nare Sai Chaitanya <62926225+naresaichaitanya@users.noreply.github.com>
2023-01-12 17:04:33 +0530

    implemented disableSslVerification in TAPClient
    - disabled the ssl host verification on TAPClient
    - added tap username and password in config files
    - updated FQDN port number in test data sql file

--- azure-functions/src/main/java/com/function/clients/TAPClient.java
+++ azure-functions/src/main/java/com/function/clients/TAPClient.java
@@ -4,6 +4,10 @@ import com.function.exceptions.ADException;
 import com.microsoft.azure.functions.ExecutionContext;
 import org.json.JSONObject;
 
+import javax.net.ssl.*;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 
 public class TAPClient {
@@ -17,13 +21,15 @@ public class TAPClient {
      * @throws Exception
      */
     static public String getAccessToken(String tapURL, ExecutionContext context) throws Exception {
-        String url = tapURL + "/api/login"; 
+        String url = tapURL + "/api/login";
         JSONObject body = new JSONObject();
         body.put("username", System.getenv("TAP_USERNAME"));
         body.put("password", System.getenv("TAP_PASSWORD"));
         String bodyAsString = body.toString();
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
+        // disable SSL host verification
+        TAPClient.disableSslVerification(context);
         JSONObject response = HttpClient.post(url, bodyAsString, headers);
         if (response.has("error") || (response.has("success") && !response.getBoolean("success"))) {
             context.getLogger().severe("Request params: " + bodyAsString);
@@ -53,12 +59,14 @@ public class TAPClient {
      * @throws Exception
      */
     static public JSONObject getDetailedReport(final String tapURL, String token, String type, String startDate,
-            String endDate, ExecutionContext context) throws Exception {
-        String url = String.format("%s/v1/spotlight/%s/report?startDate=%s&endDate=%s", tapURL, type, startDate,
+                                               String endDate, ExecutionContext context) throws Exception {
+        final String url = String.format("%s/v1/spotlight/%s/report?startDate=%s&endDate=%s", tapURL, type, startDate,
                 endDate);
-        context.getLogger().info("url: " + url);
+        context.getLogger().info("TAP detailed report endpoint: " + url);
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + token);
+        // disable SSL host verification
+        TAPClient.disableSslVerification(context);
         JSONObject response = HttpClient.get(url, headers);
         if (response != null && (response.has("error"))) {
             context.getLogger()
@@ -69,4 +77,45 @@ public class TAPClient {
         return response;
     }
 
+    /**
+     * disable SSL host name
+     * @param context: ExecutionContext
+     */
+    public static void disableSslVerification(ExecutionContext context) {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    context.getLogger().info("Disabled SSL host name");
+                    return true;
+                }
+            };
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            context.getLogger().info("Error while disabling SSL host name");
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            context.getLogger().info("Error while disabling SSL host name");
+            e.printStackTrace();
+        }
+    }
 }
