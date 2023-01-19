efe7700ed9a70bd7e331c41c6a00b33c7181ced6
VictorTorricoS <vtorrico@aitbol.com>
2023-01-19 10:01:47 -0400

    added unit test and fixed some issue's

--- ui/src/app/model/test-reports.model.ts
+++ ui/src/app/model/test-reports.model.ts
@@ -2,7 +2,6 @@ export interface ITestReports {
     reportType: string, 
     endTime: string,
     startTime: string
-    status: string
 }
 
 export interface TestReportsAPIResponse {
---
+++ ui/src/test/mock/services/ctaas-test-reports.service.mock.ts
@@ -0,0 +1,80 @@
+import { Observable } from "rxjs";
+import { ITestReports } from "src/app/model/test-reports.model";
+
+const MOCK_TEST_REPORT_A: ITestReports = {
+    reportType: "Daily-FeatureFunctionality",
+    endTime: "230110034409",
+    startTime: "230109113617"
+};
+const MOCK_TEST_REPORT_B: ITestReports = {
+    reportType: "Daily-FeatureFunctionality",
+    endTime: "230110034409",
+    startTime: "230109113617"
+};
+const MOCK_TEST_REPORT_C: ITestReports = {
+    reportType: "Daily-CallingReliability",
+    endTime: "230110034409",
+    startTime: "230109113617"
+};
+const MOCK_TEST_REPORT_D: ITestReports = {
+    reportType: "Daily-FeatureFunctionality",
+    endTime: "230110034409",
+    startTime: "230109113617"
+};
+const MOCK_TEST_REPORT_E: ITestReports = {
+    reportType: "Daily-CallingReliability",
+    endTime: "230110034409",
+    startTime: "230109113617"
+};
+
+const TEST_REPORTS_LIST = {
+    reports: [
+       
+        MOCK_TEST_REPORT_B,
+        MOCK_TEST_REPORT_D,
+        MOCK_TEST_REPORT_A,
+        MOCK_TEST_REPORT_E,
+        MOCK_TEST_REPORT_C
+    ]
+};
+
+const TEST_REPORTS_LIST_ASC = {
+    reports: [
+        
+        MOCK_TEST_REPORT_E,
+        MOCK_TEST_REPORT_C,
+        MOCK_TEST_REPORT_B,
+        MOCK_TEST_REPORT_D,
+        MOCK_TEST_REPORT_A
+    ]
+};
+
+const TEST_REPORTS_LIST_DESC = {
+    reports: [
+       
+        MOCK_TEST_REPORT_B,
+        MOCK_TEST_REPORT_D,
+        MOCK_TEST_REPORT_A,
+        MOCK_TEST_REPORT_E,
+        MOCK_TEST_REPORT_C
+    ]
+};
+
+export const TestReportsServiceMock = {
+    testReportsValue: TEST_REPORTS_LIST,
+    testReportsSortedAsc: TEST_REPORTS_LIST_ASC,
+    testReportsSortedDesc: TEST_REPORTS_LIST_DESC,
+    mockTestReportA: MOCK_TEST_REPORT_A,
+
+    getTestReportsList: () => {
+        return new Observable((observer) => {
+            observer.next(
+                JSON.parse(JSON.stringify(TEST_REPORTS_LIST))
+            );
+            observer.complete();
+            return {
+                unsubscribe() { }
+            };
+        });
+    }
+}
