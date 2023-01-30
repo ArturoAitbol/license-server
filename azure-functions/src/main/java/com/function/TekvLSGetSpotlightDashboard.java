4ef2ff4cbec06c4ab43eae070f86dce6c2345263
Kaushik Nalla <62894799+kaushiknalla@users.noreply.github.com>
2023-01-30 18:06:40 +0530

    Implemented API for Embeded PowerBi Dashboard

--- azure-functions/src/main/java/com/function/TekvLSGetSpotlightDashboard.java
+++ azure-functions/src/main/java/com/function/TekvLSGetSpotlightDashboard.java
@@ -1,23 +1,35 @@
 package com.function;
 
-import static com.function.auth.RoleAuthHandler.*;
-import static com.function.auth.Roles.*;
+import static com.function.auth.RoleAuthHandler.LOG_MESSAGE_FOR_FORBIDDEN;
+import static com.function.auth.RoleAuthHandler.LOG_MESSAGE_FOR_INVALID_SUBACCOUNT_ID;
+import static com.function.auth.RoleAuthHandler.LOG_MESSAGE_FOR_UNAUTHORIZED;
+import static com.function.auth.RoleAuthHandler.MESSAGE_FOR_FORBIDDEN;
+import static com.function.auth.RoleAuthHandler.MESSAGE_FOR_UNAUTHORIZED;
+import static com.function.auth.RoleAuthHandler.MESSAGE_SUBACCOUNT_ID_NOT_FOUND;
+import static com.function.auth.RoleAuthHandler.evaluateRoles;
+import static com.function.auth.RoleAuthHandler.getEmailFromToken;
+import static com.function.auth.RoleAuthHandler.getRolesFromToken;
+import static com.function.auth.RoleAuthHandler.getTokenClaimsFromHeader;
+import static com.function.auth.RoleAuthHandler.hasPermission;
+import static com.function.auth.Roles.CUSTOMER_FULL_ADMIN;
+import static com.function.auth.Roles.SUBACCOUNT_ADMIN;
+import static com.function.auth.Roles.SUBACCOUNT_STAKEHOLDER;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.function.auth.Resource;
+import com.function.clients.PowerBIClient;
 import com.function.db.QueryBuilder;
 import com.function.db.SelectQueryBuilder;
+import com.function.util.FeatureToggles;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -30,21 +42,21 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import io.jsonwebtoken.Claims;
 
-public class TekvLSGetAllCtaasSetups {
+public class TekvLSGetSpotlightDashboard {
 	/**
-	* This function listens at endpoint "/v1.0/ctaasSetups?subaccountId={subaccountId}". Two ways to invoke it using "curl" command in bash:
-	* 1. curl -d "HTTP Body" {your host}/v1.0/ctaasSetups?subaccountId={subaccountId}
-	* 2. curl "{your host}/v1.0/ctaasSetups"
+	* This function listens at endpoint "/v1.0/spotlightDashboard?subaccountId={subaccountId}". Two ways to invoke it using "curl" command in bash:
+	* 1. curl -d "HTTP Body" {your host}/v1.0/spotlightDashboard?subaccountId={subaccountId}
+	* 2. curl "{your host}/v1.0/spotlightDashboard"
 	*/
-	@FunctionName("TekvLSGetAllCtaasSetups")
+	@FunctionName("TekvLSGetSpotlightDashboard")
 		public HttpResponseMessage run(
 		@HttpTrigger(
 		name = "req",
 		methods = {HttpMethod.GET},
 		authLevel = AuthorizationLevel.ANONYMOUS,
-		route = "ctaasSetups/{id=EMPTY}")
+		route = "spotlightDashboard/{subaccountId=EMPTY}")
 		HttpRequestMessage<Optional<String>> request,
-		@BindingName("id") String id,
+		@BindingName("subaccountId") String subaccountId,
 		final ExecutionContext context) 
 	{
 
@@ -56,17 +68,21 @@ public class TekvLSGetAllCtaasSetups {
 			json.put("error", MESSAGE_FOR_UNAUTHORIZED);
 			return request.createResponseBuilder(HttpStatus.UNAUTHORIZED).body(json.toString()).build();
 		}
-		if(!hasPermission(roles, Resource.GET_ALL_CTAAS_SETUPS)){
+		if(!hasPermission(roles, Resource.GET_CTAAS_DASHBOARD)){
 			JSONObject json = new JSONObject();
 			context.getLogger().info(LOG_MESSAGE_FOR_FORBIDDEN + roles);
 			json.put("error", MESSAGE_FOR_FORBIDDEN);
 			return request.createResponseBuilder(HttpStatus.FORBIDDEN).body(json.toString()).build();
 		}
 
-		context.getLogger().info("Entering TekvLSGetAllCtaasSetups Azure function");
-		// Get query parameters
-		context.getLogger().info("URL parameters are: " + request.getQueryParameters());
-		String subaccountId = request.getQueryParameters().getOrDefault("subaccountId", "");
+		context.getLogger().info("Entering TekvLSGetSpotlightDashboard Azure function");
+		
+		if (subaccountId.equals("EMPTY") || subaccountId.isEmpty()){
+			context.getLogger().info( MESSAGE_SUBACCOUNT_ID_NOT_FOUND + subaccountId);
+			JSONObject json = new JSONObject();
+			json.put("error",MESSAGE_SUBACCOUNT_ID_NOT_FOUND);
+			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(json.toString()).build();
+		}
   
 		// Build SQL statement
 		SelectQueryBuilder queryBuilder = new SelectQueryBuilder("SELECT * FROM ctaas_setup");
@@ -77,26 +93,17 @@ public class TekvLSGetAllCtaasSetups {
 		String currentRole = evaluateRoles(roles);
 		switch (currentRole){
 			case CUSTOMER_FULL_ADMIN:
-				queryBuilder.appendCustomCondition("subaccount_id IN (SELECT s.id FROM subaccount s, customer_admin ca " +
-						"WHERE s.customer_id = ca.customer_id AND admin_email = ?)", email);
 				verificationQueryBuilder = new SelectQueryBuilder("SELECT s.id FROM subaccount s, customer_admin ca");
 				verificationQueryBuilder.appendCustomCondition("s.customer_id = ca.customer_id AND admin_email = ?", email);
 				break;
 			case SUBACCOUNT_ADMIN:
 			case SUBACCOUNT_STAKEHOLDER:
-				queryBuilder.appendCustomCondition("subaccount_id = (SELECT subaccount_id FROM subaccount_admin WHERE subaccount_admin_email = ?)", email);
 				verificationQueryBuilder = new SelectQueryBuilder("SELECT subaccount_id FROM subaccount_admin");
 				verificationQueryBuilder.appendEqualsCondition("subaccount_admin_email", email);
 				break;
 		}
 
-		if (id.equals("EMPTY")){
-			if (!subaccountId.isEmpty()) {
-				queryBuilder.appendEqualsCondition("subaccount_id", subaccountId, QueryBuilder.DATA_TYPE.UUID);
-			}
-		}else{
-			queryBuilder.appendEqualsCondition("id", id, QueryBuilder.DATA_TYPE.UUID);
-		}
+		queryBuilder.appendEqualsCondition("subaccount_id", subaccountId, QueryBuilder.DATA_TYPE.UUID);
 
 		if (verificationQueryBuilder != null) {
 			if (currentRole.equals(SUBACCOUNT_ADMIN) || currentRole.equals(SUBACCOUNT_STAKEHOLDER))
@@ -117,13 +124,13 @@ public class TekvLSGetAllCtaasSetups {
 			ResultSet rs;
 			JSONObject json = new JSONObject();
 
-			if (verificationQueryBuilder != null && id.equals("EMPTY") && !subaccountId.isEmpty()) {
+			if (verificationQueryBuilder != null) {
 				try (PreparedStatement verificationStmt = verificationQueryBuilder.build(connection)) {
 					context.getLogger().info("Execute SQL role verification statement: " + verificationStmt);
 					rs = verificationStmt.executeQuery();
 					if (!rs.next()) {
-						context.getLogger().info(LOG_MESSAGE_FOR_INVALID_ID + email);
-						json.put("error", MESSAGE_FOR_INVALID_ID);
+						context.getLogger().info(MESSAGE_SUBACCOUNT_ID_NOT_FOUND + email);
+						json.put("error", MESSAGE_SUBACCOUNT_ID_NOT_FOUND);
 						return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(json.toString()).build();
 					}
 				}
@@ -133,28 +140,25 @@ public class TekvLSGetAllCtaasSetups {
 			context.getLogger().info("Execute SQL statement: " + selectStmt);
 			rs = selectStmt.executeQuery();
 			// Return a JSON array of ctaas_setups
-			JSONArray array = new JSONArray();
-			while (rs.next()) {
-				JSONObject item = new JSONObject();
-				item.put("id", rs.getString("id"));
-				item.put("subaccountId", rs.getString("subaccount_id"));
-				item.put("status", rs.getString("status"));
-				item.put("azureResourceGroup", rs.getString("azure_resource_group"));
-				item.put("tapUrl", rs.getString("tap_url"));
-				item.put("onBoardingComplete", rs.getBoolean("on_boarding_complete"));
-				item.put("powerBiWorkspaceId", rs.getString("powerBi_workspace_id"));
-				item.put("powerBiReportId", rs.getString("powerBi_report_id"));
-				array.put(item);
+			JSONObject item = null;
+			if (rs.next()) {
+				item = new JSONObject();
+				item.put("powerBiWorkspaceId", rs.getString("powerbi_workspace_id"));
+				item.put("powerBiReportId", rs.getString("powerbi_report_id"));
 			}
 
-			if(!id.equals("EMPTY") && array.isEmpty()){
-				context.getLogger().info( LOG_MESSAGE_FOR_INVALID_ID + email);
-				List<String> customerRoles = Arrays.asList(DISTRIBUTOR_FULL_ADMIN,CUSTOMER_FULL_ADMIN,SUBACCOUNT_ADMIN, SUBACCOUNT_STAKEHOLDER);
-				json.put("error",customerRoles.contains(currentRole) ? MESSAGE_FOR_INVALID_ID : MESSAGE_ID_NOT_FOUND);
+			if(item == null){
+				context.getLogger().info( LOG_MESSAGE_FOR_INVALID_SUBACCOUNT_ID + email);
+				json.put("error",MESSAGE_SUBACCOUNT_ID_NOT_FOUND);
 				return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(json.toString()).build();
 			}
+			
+			JSONObject powerBiInfo = new JSONObject();
 
-			json.put("ctaasSetups", array);
+			if(FeatureToggles.INSTANCE.isFeatureActive("powerBi-dashboard"))
+				powerBiInfo = PowerBIClient.getPowerBiDetails(item.getString("powerBiWorkspaceId"), item.getString("powerBiReportId"), context);
+
+			json.put("powerBiInfo", powerBiInfo);
 			return request.createResponseBuilder(HttpStatus.OK).header("Content-Type", "application/json").body(json.toString()).build();
 		}
 		catch (SQLException e) {
