5401af395c27c290b7dd87d6743c98b5e1981219
Nare Sai Chaitanya <62926225+naresaichaitanya@users.noreply.github.com>
2023-02-01 12:09:24 +0530

    Update TekvLSModifyCtaasSetupById.java
    - fixed powerbi column names in the db

--- azure-functions/src/main/java/com/function/TekvLSModifyCtaasSetupById.java
+++ azure-functions/src/main/java/com/function/TekvLSModifyCtaasSetupById.java
@@ -233,8 +233,8 @@ public class TekvLSModifyCtaasSetupById {
         TAP_URL("tapUrl", "tap_url", QueryBuilder.DATA_TYPE.VARCHAR),
         STATUS("status", "status", QueryBuilder.DATA_TYPE.VARCHAR),
         ON_BOARDING_COMPLETE("onBoardingComplete", "on_boarding_complete", QueryBuilder.DATA_TYPE.BOOLEAN),
-    	POWERBI_WORKSPACE_ID("powerBiWorkspaceId", "powerBi_workspace_id", QueryBuilder.DATA_TYPE.VARCHAR),
-    	POWERBI_REPORT_ID("powerBiReportId", "powerBi_report_id", QueryBuilder.DATA_TYPE.VARCHAR);
+    	POWERBI_WORKSPACE_ID("powerBiWorkspaceId", "powerbi_workspace_id", QueryBuilder.DATA_TYPE.VARCHAR),
+    	POWERBI_REPORT_ID("powerBiReportId", "powerbi_report_id", QueryBuilder.DATA_TYPE.VARCHAR);
         private final String jsonAttrib;
         private final String columnName;
 
