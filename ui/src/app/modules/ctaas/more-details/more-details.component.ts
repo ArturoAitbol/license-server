31a2c9616494e20f8a9d02ae62c65052b2beb34e
sana-05 <112489748+sana-05@users.noreply.github.com>
2023-02-02 19:54:41 +0530

    Fix for report name issue - TP360-1260

--- ui/src/app/modules/ctaas/more-details/more-details.component.ts
+++ ui/src/app/modules/ctaas/more-details/more-details.component.ts
@@ -310,7 +310,7 @@ export class MoreDetailsComponent implements OnInit {
   private downloadExcelFile(data: any): void {
     const currentDate = new Date();
     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDay().toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
     const year = currentDate.getFullYear().toString();
     const hh = currentDate.getHours();
     const mm = currentDate.getMinutes();
