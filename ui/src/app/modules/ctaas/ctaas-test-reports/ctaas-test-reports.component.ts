c5bcd06ff753bb2c7856ce7756fd3166453f0d1b
Oscar Gonzalez <oscar_sgc@icloud.com>
2023-03-01 16:36:53 -0400

    Initial updates

--- ui/src/app/modules/ctaas/ctaas-test-reports/ctaas-test-reports.component.ts
+++ ui/src/app/modules/ctaas/ctaas-test-reports/ctaas-test-reports.component.ts
@@ -19,27 +19,21 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./ctaas-test-reports.component.css']
 })
 export class CtaasTestReportsComponent implements OnInit {
-  tableMaxHeight: number;
-  displayedColumns: any[] = [];
-  actionMenuOptions: any = [];
-  toggleStatus= false;
-  isLoadingResults = false;
-  isRequestCompleted = false;
-  testReportsData: any = [];
-  testReportsDataBK: any = [];
   currentCustomer: any;
   selectedTypeFilter: any = '';
-  selectedDateFilter: any = '';
-  public date: Date;
+  // selectedDateFilter: any = '';
+  public startDate: Date;
+  public endDate: Date;
   public maxDate: any;
+  public minEndDate: any;
   private subaccountDetails: any;
   readonly VIEW_REPORT = 'View Report';
 
   readonly options = {
-    VIEW_REPORT:this.VIEW_REPORT
+    VIEW_REPORT: this.VIEW_REPORT
   }
 
-  readonly reportsTypes = ['None','Daily-FeatureFunctionality', 'Daily-CallingReliability'];
+  readonly reportsTypes = ['None', 'Daily-FeatureFunctionality', 'Daily-CallingReliability'];
 
   filterForm = this.fb.group({
     typeFilterControl: [''],
@@ -55,132 +49,60 @@ export class CtaasTestReportsComponent implements OnInit {
     private testReportsService: TestReportsService,
     private fb: FormBuilder) { }
 
-  private calculateTableHeight() {
-    this.tableMaxHeight = window.innerHeight // doc height
-      - (window.outerHeight * 0.01 * 2) // - main-container margin
-      - 60 // - route-content margin
-      - 20 // - dashboard-content padding
-      - 30 // - table padding
-      - 32 // - title height
-      - (window.outerHeight * 0.05 * 2); // - table-section margin
-  }
-
-  initColumns(): void {
-    this.displayedColumns = [
-      { name: 'Report Type', dataKey: 'reportType', position: 'left', isSortable: true },
-      { name: 'Start Time', dataKey: 'startDate', position: 'left', isSortable: true },
-      { name: 'End Time', dataKey: 'endDate', position: 'left', isSortable: true }
-    ];
-  }
-
-  private getActionMenuOptions() {
-    const roles: string[] = this.msalService.instance.getActiveAccount().idTokenClaims["roles"];
-    this.actionMenuOptions = Utility.getTableOptions(roles, this.options, "testReportsOptions")
-  }
-
   ngOnInit(): void {
     this.subaccountDetails = this.subaccountService.getSelectedSubAccount();
     this.maxDate = moment().toDate()
-    this.calculateTableHeight();
-    this.getActionMenuOptions();
-    this.initColumns();
-    this.fetchTestReports();
   }
 
-  private fetchTestReports(): void {
-    this.isRequestCompleted = false;
-    this.isLoadingResults = true;
-    let reportListWithDates = []
-    this.testReportsService.getTestReportsList(this.subaccountDetails.id,this.selectedTypeFilter, this.selectedDateFilter)
-    .pipe(
-      map(((r:any):{reports : ITestReports[]} => {
-        const {reports} = r
-        try {
-          reports.forEach((report: ITestReports, index) => {
-            let parsedStartTime = moment(report.startTime, "YYMMDD HH:mm:ss UTC").format("MM-DD-YYYY HH:mm:ss UTC");
-            let parsedEndTime = moment(report.endTime, "YYMMDD HH:mm:ss UTC").format("MM-DD-YYYY HH:mm:ss UTC");
-            reportListWithDates[index] = {...report, endDate:parsedEndTime, startDate:parsedStartTime}
-          });
-          return r;
-        }catch(exception){
-          console.error('error while fetching data', exception);
-        }
-       })
-      )
-    ).subscribe(() => {
-      this.isRequestCompleted = true; 
-      this.isLoadingResults = false;
-      this.testReportsData = reportListWithDates.sort((a: any, b: any) => b['endDate'].localeCompare(a['endDate']));
-      this.testReportsDataBK = this.testReportsData;
-    }, error => {
-      console.debug('error', error);
-      this.isLoadingResults = false;
-      this.isRequestCompleted = true;
-  });
-  }
+  // rowAction(object: { selectedRow: any, selectedOption: string, selectedIndex: string }) {
+  //   const { selectedRow, selectedOption, selectedIndex } = object;
+  //   switch (selectedOption) {
+  //     case this.VIEW_REPORT:
+  //       this.onClickMoreDetails(selectedIndex);
+  //       break;
+  //   }
+  // }
 
-
-  sortData(sortParameters: Sort): any[] {
-    const keyName = sortParameters.active;
-    if (sortParameters.direction === 'asc') {
-      this.testReportsData = this.testReportsData.sort((a: any, b: any) => a[keyName].localeCompare(b[keyName]));
-    } else if (sortParameters.direction === 'desc') {
-      this.testReportsData = this.testReportsData.sort((a: any, b: any) => b[keyName].localeCompare(a[keyName]));
-    } else {
-      return this.testReportsData = this.testReportsData;
-    }
-  }
-
-  rowAction(object: { selectedRow: any, selectedOption: string, selectedIndex: string }) {
-    const { selectedRow, selectedOption, selectedIndex } = object;
-    switch (selectedOption) {
-    case this.VIEW_REPORT:
-      this.onClickMoreDetails(selectedIndex);
-      break;
-    }
-  }
-
-  toggleOptionValue(type:any){
-    switch(type){
+  toggleOptionValue(type: any) {
+    switch (type) {
       case 'Daily-FeatureFunctionality':
         this.selectedTypeFilter = 'feature';
-        this.testReportsData = [];
-        this.fetchTestReports();
         break;
       case 'Daily-CallingReliability':
         this.selectedTypeFilter = 'calling';
-        this.testReportsData = [];
-        this.fetchTestReports();
         break;
       case 'None':
-        this.selectedTypeFilter = '';
-        this.testReportsData = [];
-        this.fetchTestReports();
+        this.selectedTypeFilter = ''
         break;
       default:
         break;
     }
   }
 
-  onClickMoreDetails(index: string): void {
-    const { reportType, startTime, endTime } = this.testReportsData[index];
-    const type = (reportType === 'Daily-FeatureFunctionality') ? ReportType.DAILY_FEATURE_FUNCTIONALITY : (reportType === 'Daily-CallingReliability') ? ReportType.DAILY_CALLING_RELIABILITY : '';
-    const url = `${environment.BASE_URL}/#/spotlight/details?subaccountId=${this.subaccountDetails.id}&type=${type}&start=${startTime}&end=${endTime}`;
+  onClickMoreDetails(): void {
+
+    // const type = (reportType === 'Daily-FeatureFunctionality') ? ReportType.DAILY_FEATURE_FUNCTIONALITY : (reportType === 'Daily-CallingReliability') ? ReportType.DAILY_CALLING_RELIABILITY : '';
+    const type = ReportType.DAILY_FEATURE_FUNCTIONALITY;
+    const url = `${environment.BASE_URL}/#/spotlight/details?subaccountId=${this.subaccountDetails.id}&type=${type}&start=${this.startDate}&end=${this.endDate}`;
     window.open(url);
     window.close();
   }
 
-  toggleDateValue(date:any){
-    this.selectedDateFilter = date.value.toJSON();
-    this.testReportsData = [];
-    this.fetchTestReports();
+  toggleDateValue(date: any) {
+    console.log(date);
+    console.log(this.startDate);
+    // this.minEndDate = '';
+    // this.endDate = null;
   }
 
-  clearDateFilter() {
-    this.date = null;
-    this.selectedDateFilter = '';
-    this.testReportsData = [];
-    this.fetchTestReports();
+  clearDateFilter(selector: string) {
+    if (selector === 'start') {
+      this.startDate = null;
+      // if (this.endDate < this.minEndDate)
+      //   this.endDate = null;
+      // this.minEndDate = '';
+    } else
+      this.endDate = null;
   }
 
   ngOnDestroy() {
@@ -188,11 +110,4 @@ export class CtaasTestReportsComponent implements OnInit {
     this.unsubscribe.complete();
   }
 
-  restoreTable(event:any) {
-    if(event.data === null){
-      this.selectedDateFilter = '';
-      this.testReportsData = [];
-      this.fetchTestReports();
-    }
-  }
 }
