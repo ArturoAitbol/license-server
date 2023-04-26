df77ae0bb131b6da02f5f7851cad79b8b45203a0
VictorTorricoS <vtorrico@aitbol.com>
2023-04-26 16:29:03 -0400

    added callback ui unit tests

--- ui/src/app/modules/ctaas/callback/callback.component.spec.ts
+++ ui/src/app/modules/ctaas/callback/callback.component.spec.ts
@@ -1,25 +1,65 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CallbackComponent } from "./callback.component";
+import { TestBedConfigBuilder } from "src/test/mock/TestBedConfigHelper.mock";
+import { DialogService } from "src/app/services/dialog.service";
+import { BannerComponent } from "../banner/banner.component";
+import { DialogServiceMock } from "src/test/mock/services/dialog-service.mock";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { StakeHolderService } from "src/app/services/stake-holder.service";
+import { StakeHolderServiceMock } from "src/test/mock/services/ctaas-stakeholder-service.mock";
+import { SubaccountServiceMock } from "src/test/mock/services/subaccount-service.mock";
+import { SnackBarServiceMock } from "src/test/mock/services/snack-bar-service.mock";
+import { CallbackServiceMock } from "src/test/mock/services/callback-service.mock";
 
-// import { CallbackComponent } from './callback.component';
+let ctaasCallbackComponent: CallbackComponent;
+let fixture: ComponentFixture<CallbackComponent>;
+const dialogMock = new DialogServiceMock();
 
-// describe('CallbackComponent', () => {
-//   let component: CallbackComponent;
-//   let fixture: ComponentFixture<CallbackComponent>;
+const configBuilder = new TestBedConfigBuilder().useDefaultConfig(CallbackComponent);
+configBuilder.addProvider({ provide: DialogService, useValue: dialogMock });
+configBuilder.addProvider({ provide: MAT_DIALOG_DATA, useValue: {
+    name: 'testDemoR',
+    email: 'testdemo.r@tekvizion.com',
+    companyName: 'tekVizion',
+    jobTitle: 'Engineer',
+    phoneNumber: '9012345680',
+    subaccountId: '6b06ef8d-5eb6-44c3-bf61-e78f8644767e',
+    notifications: ['TYPE:High level', 'DAILY_REPORTS', 'MONTHLY_REPORTS']} 
+});
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ CallbackComponent ]
-//     })
-//     .compileComponents();
-//   });
+const defaultTestBedConfig = configBuilder.getConfig();
+const beforeEachFunction = () => {
+    TestBed.configureTestingModule(defaultTestBedConfig);
+    fixture = TestBed.createComponent(CallbackComponent);
+    ctaasCallbackComponent = fixture.componentInstance;
+    ctaasCallbackComponent.ngOnInit();
+    spyOn(SubaccountServiceMock, 'getSelectedSubAccount').and.returnValue({
+            id: "eea5f3b8-37eb-41fe-adad-5f94da124a5a",
+            name: "testv2Demo",
+            customerId: "157fdef0-c28e-4764-9023-75c06daad09d",
+            services: "tokenConsumption,spotlight",
+            testCustomer: false,
+            companyName:"testComp",
+            customerName:"testName"
+        });
+}
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CallbackComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+fdescribe('Callback function', () => {
+    beforeEach(beforeEachFunction);
+    it('should make a callback to "myself"', () => {
+        spyOn(StakeHolderServiceMock, 'getStakeholderList').and.callThrough();
+        spyOn(SnackBarServiceMock, 'openSnackBar').and.callThrough();
+        spyOn(CallbackServiceMock, 'createCallback').and.callThrough();
+        spyOn(dialogMock, 'close').and.callThrough();
+        spyOn(dialogMock, 'confirmDialog').and.callThrough();
+        const confirm = fixture.nativeElement.querySelector('#confirm');
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+
+        ctaasCallbackComponent.option = 'myself';
+        confirm.click();
+        dialogMock.setExpectedConfirmDialogValue(true);
+
+        expect(CallbackServiceMock.createCallback).toHaveBeenCalled();
+        //expect(SnackBarServiceMock.openSnackBar).toHaveBeenCalledWith('Call request has been made!', '');
+    });
+});
--- ui/src/test/mock/services/callback-service.mock.ts
+++ ui/src/test/mock/services/callback-service.mock.ts
@@ -16,18 +16,13 @@ export const CallbackServiceMock = {
         return new Observable((observer) => {
             observer.next(
                 {
-                    body:[
-                        {
-                            name:'testName3', 
-                            jobTitle:'testJob3', 
-                            companyName:'testCom3', 
-                            subaccountAdminEmail:'test3@gmail.com', 
-                            phoneNumber:'2222222222', 
-                            notifications: 'TYPE:LOW TIER',
-                            subaccountId: 'f6c0e45e-cfdc-4c1a-820e-bef6a856aaea',
-                            type:'High level'
-                        }
-                    ]
+                    name: 'testDemoR',
+                    email: 'testdemo.r@tekvizion.com',
+                    companyName: 'tekVizion',
+                    jobTitle: 'Engineer',
+                    phoneNumber: '9012345680',
+                    subaccountId: '6b06ef8d-5eb6-44c3-bf61-e78f8644767e',
+                    notifications: ['TYPE:High level', 'DAILY_REPORTS', 'MONTHLY_REPORTS']
                 }
             );
             observer.complete();
