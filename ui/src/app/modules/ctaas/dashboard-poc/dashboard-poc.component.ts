595d8760410af9bf194d5216cfed0ebfe1283519
ariel <ariel_arc.95@hotmail.com>
2023-05-05 16:49:56 -0400

    Replace simpleChart API for callsStatusSummary
    - Return all the reports in a single request: callingReliability and  featureFunctionality
    
    - Add new indicators to the response: offNet, onNet and p2p

--- ui/src/app/modules/ctaas/dashboard-poc/dashboard-poc.component.ts
+++ ui/src/app/modules/ctaas/dashboard-poc/dashboard-poc.component.ts
@@ -31,10 +31,10 @@ export class DashboardPocComponent implements OnInit{
   calls = { total: 0, failed: 0 };
 
   // Calling Reliabilty gaguge variables
-  callingReliability = { value: 0, total: 0, period: '' };
+  callingReliability = { value: 0, total: 0, p2p:0, onNet:0, offNet:0, period: '' };
 
   // Feature Functionality gaguge variables
-  featureFunctionality = { value: 0, total: 0, period: '' };
+  featureFunctionality = { value: 0, total: 0, p2p:0, onNet:0, offNet:0,  period: '' };
 
   // Feature Functionality gaguge variables
   vq = { period: '', calls: 0, streams: 0 };
@@ -93,6 +93,7 @@ export class DashboardPocComponent implements OnInit{
   reloadCharts() {
     const selectedDate = this.filters.get('date').value;
     this.dailyNetworkTrends.date = selectedDate;
+    this.dailyNetworkTrends.isLoading = true;
     this.dailyNetworkTrends.loadCharts();
     this.loadCharts();
   }
@@ -103,7 +104,7 @@ export class DashboardPocComponent implements OnInit{
     const obs = [];
     const selectedDate = this.filters.get('date').value;
     const selectedUser = this.filters.get('user').value;
-    this.selectedDate = this.filters.get('date').value.format('MMMM-DD-YYYY');
+    this.selectedDate = this.filters.get('date').value.clone().utc();
 
     obs.push(this.spotlightChartsService.getCustomerNetworkQualityData(selectedDate, selectedDate, selectedUser, subaccountId));
     obs.push(this.spotlightChartsService.getCustomerNetworkQualitySummary(selectedDate, selectedDate, selectedUser, subaccountId));
@@ -149,39 +150,44 @@ export class DashboardPocComponent implements OnInit{
     const selectedUser = this.filters.get('user').value;
     this.selectedDate = this.filters.get('date').value.clone().utc();
 
-    obs.push(this.spotlightChartsService.getDailyCallingReliability(selectedDate, selectedRegion, subaccountId));
-    obs.push(this.spotlightChartsService.getDailyFeatureFunctionality(selectedDate, selectedRegion, subaccountId));
+    obs.push(this.spotlightChartsService.getDailyCallsStatusSummary(selectedDate, selectedRegion, subaccountId));
     obs.push(this.spotlightChartsService.getVoiceQualityChart(selectedDate, selectedDate, selectedRegion, subaccountId));
     obs.push(this.spotlightChartsService.getCustomerNetworkQualityData(selectedDate, selectedDate, selectedUser, subaccountId));
     obs.push(this.spotlightChartsService.getCustomerNetworkQualitySummary(selectedDate, selectedDate, selectedUser, subaccountId));
     obs.push(this.spotlightChartsService.getWeeklyCallingReliability(moment().startOf('week').add(1, 'day'), moment().endOf('week').add(1, 'day'), subaccountId));
     forkJoin(obs).subscribe((res: any) => {
       // Daily Calling reliability
-      const dailyCallingReliabiltyRes: any = res[0].series;
-      this.callingReliability.total = dailyCallingReliabiltyRes.reduce((accumulator, entry) => accumulator + entry.value, 0);
-      if (this.callingReliability.total !== 0) {
-        this.callingReliability.value = ((dailyCallingReliabiltyRes.find(entry => entry.id === 'PASSED')?.value  || 0) / this.callingReliability.total) * 100;
-      } else {
-        this.callingReliability.value = 0;
-      }
+      const dailyCallingReliabiltyRes: any = res[0].callingReliability;
+      let passedCalls = dailyCallingReliabiltyRes.callsByStatus.PASSED;
+      let failedCalls = dailyCallingReliabiltyRes.callsByStatus.FAILED;
+      this.callingReliability.total = passedCalls + failedCalls;
+      this.callingReliability.p2p = dailyCallingReliabiltyRes.callsByType.p2p;
+      this.callingReliability.onNet = dailyCallingReliabiltyRes.callsByType.onNet;
+      this.callingReliability.offNet = dailyCallingReliabiltyRes.callsByType.offNet;
+      this.callingReliability.value = (passedCalls/this.callingReliability.total)*100;
+
       this.callingReliability.period = selectedDate.format("MM-DD-YYYY 00:00:00") + " AM UTC to " + selectedDate.format("MM-DD-YYYY 11:59:59") + " PM UTC";
+      
       this.calls.total = this.calls.total + this.callingReliability.total;
-      this.calls.failed = this.calls.failed + (dailyCallingReliabiltyRes.find(entry => entry.id === 'FAILED')?.value || 0);
+      this.calls.failed = this.calls.failed + dailyCallingReliabiltyRes.callsByStatus.FAILED;
 
       // Daily Feature Functionality
-      const dailyFeatureFunctionalityRes: any = res[1].series;
-      this.featureFunctionality.total = dailyFeatureFunctionalityRes.reduce((accumulator, entry) => accumulator + entry.value, 0);
-      if (this.featureFunctionality.total !== 0) {
-        this.featureFunctionality.value = ((dailyFeatureFunctionalityRes.find(entry => entry.id === 'PASSED')?.value || 0) / this.featureFunctionality.total) * 100;
-      } else {
-        this.featureFunctionality.value = 0;
-      }
+      const dailyFeatureFunctionalityRes: any = res[0].featureFunctionality;
+      passedCalls = dailyFeatureFunctionalityRes.callsByStatus.PASSED;
+      failedCalls = dailyFeatureFunctionalityRes.callsByStatus.FAILED;
+      this.featureFunctionality.total = passedCalls + failedCalls;
+      this.featureFunctionality.p2p = dailyFeatureFunctionalityRes.callsByType.p2p;
+      this.featureFunctionality.onNet = dailyFeatureFunctionalityRes.callsByType.onNet;
+      this.featureFunctionality.offNet = dailyFeatureFunctionalityRes.callsByType.offNet;
+      this.featureFunctionality.value = (passedCalls/this.featureFunctionality.total)*100;
+
       this.featureFunctionality.period = selectedDate.format("MM-DD-YYYY 00:00:00") + " AM UTC to " + selectedDate.format("MM-DD-YYYY 11:59:59") + " PM UTC";
+      
       this.calls.total = this.calls.total + this.featureFunctionality.total;
-      this.calls.failed = this.calls.failed + (dailyFeatureFunctionalityRes.find(entry => entry.id === 'FAILED')?.value || 0);
+      this.calls.failed = this.calls.failed + dailyFeatureFunctionalityRes.callsByStatus.FAILED;
 
       // Daily Voice Quality
-      const voiceQualityRes: any = res[2];
+      const voiceQualityRes: any = res[1];
       this.vq.calls = voiceQualityRes.summary.calls;
       this.vq.streams = voiceQualityRes.summary.calls_stream;
       this.vqChartOptions.series = [ { data: voiceQualityRes.percentages } ];
@@ -193,7 +199,7 @@ export class DashboardPocComponent implements OnInit{
       this.failedCallsChartOptions.series = [(this.calls.failed / this.calls.total * 100 || 0)];
 
       // Customer Network Quality
-      this.customerNetworkQualityData = res[3];
+      this.customerNetworkQualityData = res[2];
       this.polqaChartOptions.xAxis.categories = this.customerNetworkQualityData.categories.map((category: string) => category.split(" ")[1]);
       this.polqaChartOptions.series = [
         {
@@ -206,7 +212,7 @@ export class DashboardPocComponent implements OnInit{
         }
       ];
 
-      const customerNetworkQualitySummary = res[4];
+      const customerNetworkQualitySummary = res[3];
       this.customerNetworkQualitySummary.totalCalls = customerNetworkQualitySummary.totalCalls;
       this.customerNetworkQualitySummary.aboveThreshold.jitter = customerNetworkQualitySummary.jitterAboveThld;
       this.customerNetworkQualitySummary.aboveThreshold.packetLoss = customerNetworkQualitySummary.packetLossAboveThld;
@@ -217,7 +223,7 @@ export class DashboardPocComponent implements OnInit{
       this.customerNetworkQualitySummary.overall.polqa = customerNetworkQualitySummary.minPolqa;
 
       // Weekly Feature Functionality
-      const weeklyFeatureFunctionalityData = res[5];
+      const weeklyFeatureFunctionalityData = res[4];
       this.weeklyCallingReliabilityChartOptions.xAxis.categories = weeklyFeatureFunctionalityData.categories;
       this.weeklyCallingReliabilityChartOptions.series = [
         {
@@ -239,6 +245,7 @@ export class DashboardPocComponent implements OnInit{
       const endTime = performance.now();
       this.loadingTime = (endTime - startTime) / 1000;
       this.isloading = false;
+      this.dailyNetworkTrends.isLoading = false;
       this.reloadFilterOptions();
     }, error => {
       console.error(error);
