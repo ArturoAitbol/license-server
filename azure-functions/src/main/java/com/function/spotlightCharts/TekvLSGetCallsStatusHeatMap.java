c7c5815706aac70370bd6752e5673f0b3c8651d1
Paolo Fernandez <pfernandez@aitbol.com>
2023-05-29 19:28:47 -0400

    Fixing TP360-2041 and TP360-2042

--- azure-functions/src/main/java/com/function/spotlightCharts/TekvLSGetCallsStatusHeatMap.java
+++ azure-functions/src/main/java/com/function/spotlightCharts/TekvLSGetCallsStatusHeatMap.java
@@ -80,7 +80,7 @@ public class TekvLSGetCallsStatusHeatMap {
 				"LEFT JOIN test_plan tp ON p.testplanid = tp.id " +
 				"WHERE sr.finalResult = true AND (sr.status = 'PASSED' OR sr.status = 'FAILED') " +
 				"AND (sr.failingerrortype IS NULL or trim(sr.failingerrortype) = '' or sr.failingerrortype = 'Routing Issue' or sr.failingerrortype = 'Teams Client Issue' or sr.failingerrortype = 'Media Quality' or sr.failingerrortype = 'Media Routing') " + 
-				"AND tp.name IN ('LTS','STS','POLQA')";
+				"AND tp.name IN ('" + Utils.DEFAULT_TEST_PLAN_NAMES + "')";
 
 		// Build region filter if present
 		if (!regions.isEmpty() || !users.isEmpty()) {
