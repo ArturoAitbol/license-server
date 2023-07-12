2cc8cc9a13b55ba58d784f29245df3ce8597634e
Paolo Fernandez <pfernandez@aitbol.com>
2023-07-12 15:23:55 -0400

    fixing TP360-2436

--- ui/src/app/modules/ctaas/ctaas-test-reports/search-consolidated-report/search-consolidated-report.component.ts
+++ ui/src/app/modules/ctaas/ctaas-test-reports/search-consolidated-report/search-consolidated-report.component.ts
@@ -12,17 +12,17 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./search-consolidated-report.component.css']
 })
 export class SearchConsolidatedReportComponent implements OnInit {
-  maxDate: any;
+  maxStartDate: any;
+  minStartDate: any;
   maxEndDate: any;
   minEndDate: any;
   subaccountDetails:any;
   isDataLoading = false;
   maxTime: any;
-  minTimeBK: any;
   minTime: any;
   startDate: any;
   endDate: any;
-  dateLimit: number = 9;
+  dateLimit: number = 4;
   readonly CALLING: string = 'Calling Reliability';
   readonly FEATURE: string = 'Feature Functionality';
   readonly VOICE: string = 'Voice Quality (POLQA)';
@@ -45,8 +45,8 @@ export class SearchConsolidatedReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.subaccountDetails = this.subaccountService.getSelectedSubAccount();
-    this.maxDate = moment().format("YYYY-MM-DD[T]HH:mm:ss");
-    this.maxEndDate = this.maxDate;
+    this.maxStartDate = moment().format("YYYY-MM-DD[T]HH:mm:ss");
+    this.maxEndDate = this.maxStartDate;
   }
 
   onCancel(): void {
@@ -75,21 +75,16 @@ export class SearchConsolidatedReportComponent implements OnInit {
     window.open(url);
   }
 
-  toggleDateValue(date: any) {
-    let selectedDate;
-    let actualDate;
-    let dateControl;
-    this.minEndDate = date;
+  toggleStartDate(date: any) {
     this.startDate = date;
     this.searchForm.get('startTime').setValue("");
-    selectedDate = [parseInt(moment.utc(date).format("DD")), parseInt(moment.utc(date).format("MM")), parseInt(moment.utc(date).format("YYYY"))];
-    actualDate = [parseInt(moment.utc().format("DD")),  parseInt(moment.utc().format("MM")), parseInt(moment.utc().format("YYYY"))];
-    dateControl = actualDate[0] - selectedDate[0];
-    if(dateControl < this.dateLimit && (selectedDate[1] == actualDate[1]) && (selectedDate[2] == actualDate[2])){
-      this.maxEndDate = moment.utc(date).add(dateControl, 'days').format("YYYY-MM-DD[T]HH:mm:ss");
-    }else {
-      this.maxEndDate = moment.utc(date).add(this.dateLimit, 'days').format("YYYY-MM-DD[T]HH:mm:ss");
-    }
+    this.minEndDate = date;
+    let actualDate = moment.utc();
+    let dateLimitControl = moment.utc(date).add(this.dateLimit, "days");
+    if (actualDate.isSameOrBefore(dateLimitControl))
+      this.maxEndDate = actualDate.format("YYYY-MM-DD[T]HH:mm:ss");
+    else
+      this.maxEndDate = dateLimitControl.format("YYYY-MM-DD[T]HH:mm:ss");
   }
   
   toggleEndDate(endDate: any) {
@@ -98,21 +93,19 @@ export class SearchConsolidatedReportComponent implements OnInit {
   }
 
   validateTimers() {
-    let selectedStartDate = [ parseInt(moment(this.startDate).format("DD")), parseInt(moment(this.startDate).format("MM")), parseInt(moment(this.startDate).format("YYYY"))]
-    let selectedEndDate = [ parseInt(moment(this.endDate).format("DD")), parseInt(moment(this.endDate).format("MM")), parseInt(moment(this.endDate).format("YYYY"))]
-    if(this.endDate > this.startDate){
+    let selectedStartDate = moment.utc(this.startDate);
+    let selectedEndDate = moment.utc(this.endDate);
+    if (selectedStartDate.diff(selectedEndDate, "days") > 0) {
       this.minTime = "00:00";
-    }else if(selectedStartDate[0] == selectedEndDate[0] && selectedStartDate[1] == selectedEndDate[1] && selectedStartDate[2] == selectedEndDate[2]) {
-      this.minTime = this.minTimeBK;
+      this.maxTime = "23:59";
     }
   }
   
-  onChangeStartTime(event) {
-    this.minTime = event
-    this.minTimeBK = event
+  onChangeStartTime(event: any) {
+    this.minTime = event;
   }
 
-  endtTimeChanged(event) {
-    this.maxTime = event
+  onChangeEndTime(event: any) {
+    this.maxTime = event;
   }
 }
