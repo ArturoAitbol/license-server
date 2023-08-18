1ccad444f18edeb923d6133d06441e4ac4be0c8b
Oscar <oscar_sgc@icloud.com>
2023-08-18 13:49:10 -0400

    Revert "Tp360 fix chrome driver 116 for mac"

--- tests/ui/src/main/java/ui/core/browsers/Chrome.java
+++ tests/ui/src/main/java/ui/core/browsers/Chrome.java
@@ -8,11 +8,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Chrome extends AbstractDriver {
     @Override
     public WebDriver initDriver() {
-//        System.setProperty("webdriver.chrome.driver","/Users/arturoamorosogarcia/.m2/repository/webdriver/chromedriver/mac64/116.0.5845.96/chromedriver");
-//        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--lang=en");
         options.addArguments("--remote-allow-origins=*");
+/*        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setCapability(ChromeOptions.CAPABILITY, options);
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+        options.setCapability("goog:loggingPrefs", logPrefs);*/
         return new ChromeDriver(options);
     }
 }
